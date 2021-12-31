e210c61d06e185ed93df4e5dce77d79b767b2c2a
51AN <sianashsad@iut-dhaka.edu>
2021-12-31 19:37:42 +0600

    Lab_task_06

---
+++ Lab_Task_06/Lab_Task_06/UserClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_06
+{
+    internal class UserClass : LMS
+    {
+        private string userID;
+        
+        public bool checkIfUserExists(string id)
+        {
+            if (userID == id)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void setterUser(string userid, string name, string addresss)
+        {
+            this.userID = userid;
+            this.userName = name;
+            this.address = addresss;
+
+        }
+
+        public string getName()
+        {
+            return userName;
+        }
+        public string getAdress()
+        {
+            return address;
+        }
+
+    }
+}
