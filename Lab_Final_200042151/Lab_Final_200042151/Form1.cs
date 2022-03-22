4e0bfa0f71ef983e2e90930ff6c8d4306a6d999e
51AN <sianashsad@iut-dhaka.edu>
2022-03-22 20:32:15 +0600

    comments

--- Lab_Final_200042151/Lab_Final_200042151/Form1.cs
+++ Lab_Final_200042151/Lab_Final_200042151/Form1.cs
@@ -17,9 +17,9 @@ namespace Lab_Final_200042151
         public Form1()
         {
             InitializeComponent();
-            using (var reader = new StreamReader(@"C:\Users\User\Downloads\SWE4201MarkSheet.csv"))
+            using (var reader = new StreamReader(@"C:\Users\User\Downloads\SWE4201MarkSheet.csv"))//reading CSV files
             {
-
+                //skipping the first 9 lines
                 reader.ReadLine();
                 reader.ReadLine();
                 reader.ReadLine();
@@ -33,11 +33,11 @@ namespace Lab_Final_200042151
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    var segments = line.Split(',');
+                    var segments = line.Split(',');//setting line splitter to ','
 
                     
                     studentClass student = new studentClass();
-
+                    //assigning to student class variables
                     student.id = segments[0];
                     student.name = segments[1];
                     student.attendance = segments[2];
@@ -58,7 +58,7 @@ namespace Lab_Final_200042151
                     double final;
                     double viva;
 
-
+                    //checking if variable is null, if so then taking it as 0
                     if (student.attendance != "")
                     {
                         attendance = Convert.ToDouble(student.attendance);
@@ -130,6 +130,8 @@ namespace Lab_Final_200042151
                     double percentage;
                     string grade;
                     
+
+                    //determining total quiz marks (best 3 out of 4)
                     if(quiz4 <= quiz1 && quiz4 <= quiz2 && quiz4 <= quiz3)
                     {
                         quizTotal = quiz1 + quiz2 + quiz3;
@@ -148,10 +150,13 @@ namespace Lab_Final_200042151
                     }
 
 
-                    totalMarks = attendance + quizTotal + mid + final + viva;
+                    totalMarks = attendance + quizTotal + mid + final + viva;//determining total marks
 
-                    percentage = (totalMarks / 300.0) * 100;
+                    percentage = (totalMarks / 300.0) * 100;//finding out percentage
 
+
+
+                    //Assigning grade
                     if(percentage>=80 && percentage<=100)
                     {
                         grade = "A+";
@@ -193,18 +198,19 @@ namespace Lab_Final_200042151
                         grade = "F";
                     }
 
+
+
                     student.total = Convert.ToString(totalMarks);
                     student.grade = grade;
-                    percentage = Math.Round(percentage, 2);
+                    percentage = Math.Round(percentage, 2);//rounding percentage (2 numbers from decimal point)
                     student.percentage = Convert.ToString(percentage);
-
                     student.quizTotal = Convert.ToString(quizTotal);
                     
 
-                    studentClasses.Add(student);
+                    studentClasses.Add(student);//assigning student to studentClass list
                 }
             }
-
+            //showing student result info in listbox
             for(int i=0;i<studentClasses.Count;i++)
             {
                 listBox1.Items.Add("Student ID : " + studentClasses[i].printid());;
@@ -225,17 +231,15 @@ namespace Lab_Final_200042151
             string checkID = studentIdBox.Text;
             bool errorExists = true;
 
+
+            //using LINQ to search for student
             var student = from std in studentClasses
                           where std.id == checkID
                           select std;
 
-
-
-
-                
-
             foreach(var std in student)
             {
+                //if student found, then updating information
                 attLabel.Text = "Attendance : " + std.attendance.ToString();
                 quiz1label.Text = "Quiz 1 : " + std.quiz1.ToString();
                 quiz2label.Text = "Quiz 2 : " + std.quiz2.ToString();
