054263b0cd680ad1fe0368586a8c93256d067ef5
51AN <sianashsad@iut-dhaka.edu>
2022-11-13 00:57:54 +0600

    Update after a long time

---
+++ TeachingOOP/TeachingOOP/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachingOOP
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+
+            int a;
+            int b;
+            int c;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            Adder adder = new Adder();
+
+
+            Console.WriteLine("Summation of first two integers : " + adder.add(a, b));
+            Console.WriteLine("Summation of three integers : " + adder.add(a, b, c));
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Adder
+    {
+        
+        public Adder()
+        {
+            
+        }
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+
+
+           
+    }
+
+
+
+
+
+
+
+
+}
